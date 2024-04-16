6d31ccae004de6171c89994374c3b96521e121e4
Julien Barthelemy <julien.trehen@gmail.com>
2024-04-16 13:49:00 +0200

    Updated theme

--- nvim/lua/julien/plugins/colorscheme.lua
+++ nvim/lua/julien/plugins/colorscheme.lua
@@ -10,8 +10,6 @@ return {
         }
       }
     })
-    vim.cmd('colorscheme github_dark')
-    vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-    vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+    vim.cmd('colorscheme github_dark_default')
   end,
 }
